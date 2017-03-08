cb16c51fda6fcdfcaca9d7c7cbfd629b301f5d7e
Oscar Roche <oscar.w.roche@gmail.com>
2017-03-08 16:35:59 +0100

    simplified code

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 'use strict';
 
-/*jslint unparam: true */
-
 if (!(process.env.APP_SECRET && process.env.VALIDATION_TOKEN && process.env.PAGE_ACCESS_TOKEN)) {
   console.error("Missing config values");
   process.exit(1);
 }
-
-var callSendAPI;
-
-// External Libraries
 
 var
   bodyParser = require('body-parser'),
   express = require('express'),
   request = require('request');
 
-// Including libraries for message logging
-
 var botmeterApiUrl = 'http://botmeter-proxy-staging.herokuapp.com/';
 var botmeter = require('@botfuel/botmeter-logger')(botmeterApiUrl, process.env.BOTMETER_USER_KEY).facebook;
-
-// Defining ExpressJS app
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
@@ -42,16 +32,11 @@ app.get('/webhook', function (req, res) {
 
 app.post('/webhook', function (req, res) {
   var data = req.body;
-
-  // Make sure this is a page subscription
   if (data.object === 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
     data.entry.forEach(function (pageEntry) {
-      // Iterate over each messaging event
       pageEntry.messaging.forEach(function (messagingEvent) {
         if (messagingEvent.message) {
-          callSendAPI(messagingEvent.message.text);
+          callSendAPI(messagingEvent.sender.id, messagingEvent.message.text);
         }
       });
     });
@@ -59,7 +44,15 @@ app.post('/webhook', function (req, res) {
   }
 });
 
-callSendAPI = function (originalRequest) {
+var callSendAPI = function (senderId, originalRequest) {
+  var messageData = {
+    recipient: {
+      id: senderId
+    },
+    message: {
+      text: 'Hello World',
+    }
+  };
   var requestData = {
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
@@ -68,12 +61,11 @@ callSendAPI = function (originalRequest) {
   };
 
   request(requestData, function (error, response, body) {
-    // Botmeter user.bot message logging
-    botmeterLogger.logDocument(originalRequest, messageData, function (e, d) {
+    botmeter.logDocument(originalRequest, messageData, function (e, d) {
       if (e) {
-        console.log(e);
+        console.log('BOTMETER ERROR: ', e);
       } else {
-        console.log(d);
+        console.log('BOTMETER LOGGING: ', d);
       }
     });
   });
@@ -84,5 +76,3 @@ app.listen(app.get('port'), function () {
 });
 
 module.exports = app;
-
-/*jslint unparam: false */
